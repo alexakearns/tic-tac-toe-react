8a26775c9768273411967cd3726b130756c47a79
Alexa <alexakearns@outlook.com>
2020-07-08 17:24:53 +0100

    Delete Board constructor Add game render

--- src/index.js
+++ src/index.js
@@ -12,14 +12,6 @@ function Square(props) {
 }
 
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
-    };
-  }
-
   handleClick(i) {
     const squares = this.state.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
@@ -32,8 +24,8 @@ class Board extends React.Component {
     });
   }
   renderSquare(i) {
-    return <Square value={this.state.squares[i]}
-    onClick={() => this.handleClick(i)} />;
+    return <Square value={this.props.squares[i]}
+    onClick={() => this.props.onClick(i)} />;
   }
 
   render() {
@@ -47,7 +39,6 @@ class Board extends React.Component {
 
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -79,13 +70,26 @@ class Game extends React.Component {
     }
   }
   render() {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const winner =  calculateWinner(current.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board 
+            squares={ current.squares }
+            onClick={(i) => this.handleClick(i)}
+          />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
+          <div>{ status }</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
